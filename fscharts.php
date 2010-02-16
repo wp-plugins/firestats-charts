ec4f9f2024a84449a9ae61675250575c0392e79b
mArm <mArm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 14:06:58 +0000

    forgot to upgrade version number in plugin file...
    git-svn-id: https://plugins.svn.wordpress.org/firestats-charts/tags/1.0.1@206462 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fscharts.php
+++ fscharts.php
@@ -3,7 +3,7 @@
 Plugin Name: FireStats Charts
 Plugin URI: http://wordpress.org/extend/plugins/firestats-charts/
 Description: Add a chart view to firestats's statistics. <strong>Require <a href="http://firestats.cc/" target="_blank">FireStats</a> > 1.6.3</strong>.
-Version: 1.0.0
+Version: 1.0.1
 Author: David "mArm" Ansermot
 Author URI: http://www.ansermot.ch
 */

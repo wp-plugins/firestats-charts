145aa01b3442f92b076347dbbae0f007803ea7c6
mArm <mArm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-24 06:41:00 +0000

    Tag 1.0.4 * Fixed absolute hardcoded image link always pointing on ansermot.ch (thanks Alexander) * Fixed chart image title * Cleaned some code * Fixed plugin's folder name in image path
    git-svn-id: https://plugins.svn.wordpress.org/firestats-charts/tags/1.0.4@209711 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fscharts.php
@@ -0,0 +1,498 @@
+<?php
+/*
+Plugin Name: FireStats Charts
+Plugin URI: http://wordpress.org/extend/plugins/firestats-charts/
+Description: Add a chart view to firestats's statistics. <strong>Require <a href="http://firestats.cc/" target="_blank">FireStats</a> > 1.6.3</strong>.
+Version: 1.0.4
+Author: David "mArm" Ansermot
+Author URI: http://www.ansermot.ch
+*/
+
+/*  Copyright 2010  David Ansermot  (email : dev@ansermot.ch)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+// *************************************
+// Check of PHP5 version
+// *************************************
+if (version_compare(phpversion(), '5.1', '<'))	die ('This plugin requires PHP 5.1.0 or higher.');
+
+
+// *************************************
+// Disable plugin if not in admin parts
+// *************************************
+$fsChartsDisabled = false;
+if (stripos('wp-admin', $_SERVER['HTTP_HOST'].'/'.$_SERVER['REQUEST_URI']) !== false) {
+	$fsChartsDisabled = true;
+}
+
+
+/**********************************************
+ *
+ *           Plugin's Class 
+ *
+ */
+if (!class_exists('FsCharts') && !$fsChartsDisabled) {
+
+	// *************************************
+	// Class declaration
+	// *************************************
+	class FsCharts {
+
+		// Verbose mode for debuging
+		protected $verbose = false;
+		// Internal variables
+		protected $vars = array();
+		// Plugin's key
+		protected $piKey = 'wp-fscharts-pi';
+    // Plugin's version
+    protected $version = '1.0.4';
+		
+		
+		
+		
+		
+				
+		/**********************************************
+		 *
+		 *           MAIN PART
+		 *
+		 */
+	 
+		public function __construct($verbose = null) {
+
+			// Detect verbose mode
+			if ($verbose !== null && is_bool($verbose)) {
+				$this->verbose = $verbose;
+			} else {
+				if (isset($_GET['verbose']) && !empty($_GET['verbose'])) {
+					$this->verbose = ($_GET['verbose'] === 1) ? true : false;
+				}
+				// @todo : add $_POST['verbose'] support
+			}
+
+			// Get plugin's variables GET
+			if (isset($_GET[$this->piKey]) && is_array($_GET[$this->piKey])) {
+				foreach ($_GET[$this->piKey] as $key => $value) {
+					$this->vars[$key] = $value;
+				}
+			}
+
+			// Get plugin's variables POST (override GET's variables)
+			if(isset($_POST[$this->piKey]) && is_array($_POST[$this->piKey])) {
+				foreach ($_POST[$this->piKey] as $key => $value) {
+					$this->vars[$key] = $value;
+				}
+			}
+      
+      // Include jpgraph library
+      require_once('res/jpgraph/jpgraph.php');
+			require_once('res/jpgraph/jpgraph_line.php');
+
+		}
+		
+		/**
+		 * Install le plugin
+		 *
+		 * @param 	void
+		 * @return 	void
+		 * @access 	public
+		 */
+		public function install() {
+			
+			// For 1.1.0
+			
+      // Check if pi is installed
+			$installed = get_option('fscharts_version', null);
+			
+			// If not installed, add install it
+			if ($installed === null) {
+
+        // Chart configuration
+        add_option('fscharts_height', 240);
+        add_option('fscharts_width', 500);
+
+        // Display configuration
+        add_option('fscharts_display_visits', 1);
+        add_option('fscharts_display_hits', 1);
+        add_option('fscharts_display_table', 1);
+
+        // Zoom configuration
+        add_option('fscharts_zoom', 0);
+        add_option('fscharts_zoom_height', 600);
+        add_option('fscharts_zoom_width', 800);
+
+        $installed = '1.0.0';
+
+      }
+
+      // Updates between versions
+      //
+      // if (version_compare($installed, '1.2.0', '<')) {
+      //  add_option('use_extJS', 0);
+      //  $installed = '1.2.0';
+      // }
+			
+			// Update version number
+			if ($installed === null) {
+				add_option('fscharts_version', $this->version);
+			} else {
+				update_option('fscharts_version', $this->version);
+			}
+			
+		}
+		
+		/**
+		 * Install the widget on the dashboard
+		 *
+		 * @param 	void
+		 * @return 	void
+		 * @access 	public
+     * @since   1.0.3
+		 */
+    public function installWidget() {
+
+			global $wpdb;
+			global $fscPi;
+		
+			// Check if FireStats is installed
+			if (!defined('FS_WORDPRESS_PLUGIN_VER')) {
+				echo '<p>FireStats Charts require <strong>FireStats >= 1.6.3</p>';
+			} else {
+				// Get datas from firestats table
+				$graph = new Graph(650, 380);
+				$graph->SetScale('intint'/*, 0, 0, 0, max($days) - min($days) + 1*/);
+				$graph->SetMargin(40,30,40,100);
+				$graph->title->Set('FireStats Charts '.$this->version);
+				$graph->xaxis->SetLabelAngle(90);
+				$graph->xaxis->title->Set('Days');
+				$graph->yaxis->title->Set('Hits');
+			
+				// Create the linear plot
+				$hits = $fscPi->getHits();
+				$lineplot = new LinePlot($hits['datas']);
+				$graph->xaxis->SetTickLabels($hits['labels']);
+				
+				$lineplot->SetFillColor('orange@0.5');
+				$lineplot->value->Show();
+				$lineplot->value->SetColor('darkred');
+				//$lineplot->value->SetFont('', '', 8);
+				$lineplot->value->SetFormat('%d');
+			
+				// Add the plot to the graph
+				$graph->Add($lineplot);
+				
+				// Delete too old temp files (add 1.0.2)
+				$this->cleanTempDirectory();
+				
+				// Graph key for temp file (add 1.0.2)
+				$graphKey = 'graph_'.md5(time());
+				$graph->Stroke(dirname(__FILE__).'/out/'.$graphKey.'.png');
+			
+				echo '<div style="text-align: center;"><img src="/wp-content/plugins/firestats-charts/out/'.$graphKey.'.png" alt="FireStats Graph" /></div>';
+			}
+
+    }
+		
+		
+		/**********************************************
+		 *
+		 *          DATAS PART
+		 *
+		 */
+		
+		/**
+		 * Get all hits
+		 *
+		 * @param 	void
+		 * @return 	void
+		 * @access 	public
+		 */
+		public function getHits() {
+			
+			global $wpdb;
+			
+			$ydata = array();
+  		$days = array();
+			
+      $req = 'SELECT *,count(*) AS toto FROM '.$wpdb->prefix.'firestats_hits GROUP BY SUBSTRING(timestamp,1,10)';
+      $visites = $wpdb->get_results($req);
+      foreach ($visites as $visite) {
+        $ydata[] = $visite->toto;
+        $days[] = substr($visite->timestamp, 0, 10);
+      }
+			
+      return array('datas' => $ydata, 'labels' => $days);
+			
+    }
+		
+		
+		
+		
+	
+	
+	
+	
+		
+		/**********************************************
+		 *
+		 *          ADMIN PART
+		 *
+		 */	
+		
+		
+		/**
+		 * Ajoute le menu d'admin du plugin
+		 *
+		 * @param	void
+		 * @return 	void
+		 * @access 	public
+		 */
+		public function adminMenu() {
+		
+			// In 1.1.0
+			
+			//add_options_page('Firestats Charts', 'Firestats Charts', 8, __FILE__, array(&$this, 'adminMenuOptionPage'));
+			
+			// Inscrit les JS supplémentaires
+			//wp_register_script('JQuery', get_option('home').'/wp-includes/js/jquery/jquery.js');
+			//wp_enqueue_script('JQuery');
+
+		}
+		
+		
+		/**
+		 * Affiche la page des options
+		 *
+		 * @param	void
+		 * @return 	void
+		 * @access 	public
+		 */
+		public function adminMenuOptionPage() {
+			// In 1.1.0
+		}
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		/**********************************************
+		 *
+		 *          MISC PART
+		 *
+		 */	
+		
+		
+		/**
+		 * Clean up temp directory
+		 *
+		 * @param 	int		$lifetime: La durée de vie d'un fichier 
+		 * @return 	bool
+		 * @access 	public
+		 * @since		1.0.2
+		 */
+		protected function cleanTempDirectory() {
+			
+			$directory = dirname(__FILE__).'/out/';
+			
+			// If must make dir, return true
+			if (!is_dir($directory)) {
+				mkdir($directory);
+				return true;
+			}
+			
+			// Open directory
+			if (!$dirHandler = opendir($directory)) {
+				return false;
+			}
+			
+			// List files
+			while ($file = readdir($dirHandler)) {
+			
+				if ($file != '.' &&  $file != '..' && !is_dir($directory.'/'.$file) && (substr($file, -4) == '.png')) {
+					
+					// Modification date
+					$filetime = filemtime($directory.'/'.$file);
+					
+					// Delete if too old ( > 1h00)
+					if (time() - $filetime > 3600) {
+						unlink($directory.'/'.$file);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************************
+ *
+ *           Init
+ *
+ */
+
+
+// Check if class exists and isn't set
+if (class_exists('FsCharts') && !isset($fscPi) && !isset($GLOBALS['fscPi'])) {
+
+	// Plugin initialization
+  $fscPi  = new FsCharts();
+  $GLOBALS['fscPi'] = $fscPi;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************************
+ *
+ *          Hooks management
+ *
+ */
+ 
+ 
+if (isset($fscPi)) {
+
+	// Add the widget installation
+  add_action('wp_dashboard_setup', 'fsc_install_widget');
+	
+	// Register activation function
+	register_activation_hook(__FILE__, array(&$fscPi, 'install'));
+	
+	/**
+	 * Add the widget to the dashboard
+	 *
+	 * @param		void
+	 * @return 	void
+	 * @access 	public
+	 */
+  function fsc_install_widget() {
+    wp_add_dashboard_widget('fsc_dashboard_widget', 'FireStats Charts', 'fsc_installWidget');
+  }
+	
+}
+
+/**
+ * Install the widget on the dashboard
+ *
+ * @param	void
+ * @return 	void
+ * @access 	public
+ * @since		1.0.3
+ */
+function fsc_installWidget() {
+	global $fscPi;
+	$fscPi->installWidget();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************************
+ *
+ *          Template Tags
+ *
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************************
+ *
+ *          WordPress Hacks
+ *
+ */
+
+ 
+
+?>
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== FireStats Charts ===
+Contributors: mArm
+Tags: firestats, statistics, charts, graph, stats, widget, admin
+Requires at least: 2.9.0
+Tested up to: 2.9.2
+Stable tag: 1.0.4
+
+
+This plugin adds a graphical chart of the FireStats statistics plugin on the admin dashboard
+
+== Description ==
+<p>This plugin adds a graphical chart of the FireStats statistics plugin on the admin dashboard</p>
+<p><em>Sorry for my forgots the between updates 1.0.2 => 1.0.4</em></p>
+<p><strong>Require FireStats >= 1.6.3</strong><br />
+<strong>Not tested on WPMU</strong></p>
+
+== Installation ==
+1. Upload the folder `fscharts` to the `/wp-content/plugins/` directory
+2. Set the rights on the folder `/fscharts/out` to 777 (output folder for charts images)
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Go to your dashboard to see the chart
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+1. The widget on the dashboard (version 1.0.0)
+
+== Changelog ==
+
+= 1.0.4 =
+* Fixed absolute hardcoded image link always pointing on ansermot.ch (thanks Alexander)
+* Fixed chart image title
+* Cleaned some code
+* Fixed plugin's folder name in image path
+
+= 1.0.3 =
+* Updated version number in class
+* Added temp file cleaning (should be in 1.0.2 but forgot to add the call to it)
+* Moved rendering function inside the class
+* Removed template tag
+
+= 1.0.2 =
+* Removed static graph image
+* Added temp graph png generation
+* Added temp directory file cleaning (older than 1 hour)
+
+= 1.0.1 =
+* Fixed missing FireStats
+* Tested on 2.9.2 WP
+
+= 1.0.0 =
+* First version
+* Simply display a chart on the dashboard with the hits of the last month
+More feature in the next, comming soon, versions
+
